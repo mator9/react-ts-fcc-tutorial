5dee1e1c1bb228887c04d0f3757a2280ff60bfe9
Martin Rybovic <martin.rybovic@exxeta.com>
2023-08-14 19:27:37 +0200

    added update, edit delete, use local storage

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,7 @@
+import { TodoItem } from './TodoItem'
 import { useTodo } from '../context/useTodo'
 import { SiStarship } from 'react-icons/si'
+import { motion } from 'framer-motion'
 
 export const TodoList = () => {
   const { todos } = useTodo()
@@ -16,10 +18,10 @@ export const TodoList = () => {
   }
 
   return (
-    <ul className="grid max-w-lg gap-2 px-5 m-auto">
+    <motion.ul className="grid max-w-lg gap-2 px-5 m-auto">
       {todos.map(todo => (
-        <li key={todo}>{todo}</li>
+        <TodoItem todo={todo} key={todo.id} />
       ))}
-    </ul>
+    </motion.ul>
   )
 }
--- src/context/TodoContext.tsx
+++ src/context/TodoContext.tsx
@@ -1,26 +1,77 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext } from 'react'
 import { nanoid } from 'nanoid'
 import { useLocalStorage } from 'usehooks-ts'
 
 interface TodoContextProps {
-  todos: string[]
+  todos: Todo[]
   addTodo: (text: string) => void
+  deleteTodo: (id: string) => void
+  editTodo: (id: string, text: string) => void
+  updateTodoStatus: (id: string) => void
 }
+
+export interface Todo {
+  id: string
+  text: string
+  status: 'undone' | 'completed'
+}
+
 export const TodoContext = createContext<TodoContextProps | undefined>(
   undefined,
 )
 
 export const TodoProvider = (props: { children: React.ReactNode }) => {
-  const [todos, setTodos] = useState<string[]>([])
+  const [todos, setTodos] = useLocalStorage<Todo[]>('todos', [])
 
   // ::: ADD NEW TODO :::
   const addTodo = (text: string) => {
-    setTodos([...todos, text])
+    const newTodo: Todo = {
+      id: nanoid(),
+      text,
+      status: 'undone',
+    }
+
+    setTodos([...todos, newTodo])
+  }
+
+  // ::: DELETE A TODO :::
+  const deleteTodo = (id: string) => {
+    setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id))
+  }
+
+  // ::: EDIT A TODO :::
+  const editTodo = (id: string, text: string) => {
+    setTodos(prevTodos => {
+      return prevTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, text }
+        }
+        return todo
+      })
+    })
+  }
+
+  // ::: UPDATE TODO STATUS :::
+  const updateTodoStatus = (id: string) => {
+    setTodos(prevTodos => {
+      return prevTodos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            status: todo.status === 'undone' ? 'completed' : 'undone',
+          }
+        }
+        return todo
+      })
+    })
   }
 
   const value: TodoContextProps = {
     todos,
     addTodo,
+    deleteTodo,
+    editTodo,
+    updateTodoStatus,
   }
 
   return (
